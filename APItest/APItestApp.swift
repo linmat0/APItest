15d70b2bf4b1035c06e92678fdd83f1058902cfe
Matthew Lin <Lin.M@etoncollege.org.uk>
2024-06-05 14:27:36 +0100

    Initial Commit

---
+++ APItest/APItestApp.swift
@@ -0,0 +1,17 @@
+//
+//  APItestApp.swift
+//  APItest
+//
+//  Created by Lin, Matthew (CDNP) on 05/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct APItestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
